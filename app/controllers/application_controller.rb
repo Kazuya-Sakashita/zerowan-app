cbb4920443230b7e727a20b94dd4d85a8a5471e0
Kazuya-Sakashita <kz0508@gmail.com>
2022-07-09 16:03:40 +0900

    Unpermitted parameter: :profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      profile_attributes: [:name,:address, :phoneNumber, :birthday, :breedingExperience, :user_id ]   
+      ])
+  end
 
   def after_sign_in_path_for(resource)
     root_path # ログイン後に遷移するpathを設定
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,11 +3,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+    @profile = @user.build_profile
+    super
+  end
 
   # POST /resource
   def create
@@ -19,6 +22,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # 新規追加
   def confirm
     @user = User.new(sign_up_params)
+    # @profile = @user.build_profile(@user.profile)
     if @user.valid?
       render :action => 'confirm'
     else
