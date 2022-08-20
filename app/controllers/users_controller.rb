790aeadf6093c884e92a729f067dbdc8fed9d82f
Kazuya-Sakashita <kz0508@gmail.com>
2022-08-20 16:01:52 +0900

    users_controller_spec.rb修正中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to @user
     else
-      render 'edit'
+      flash[:alert] = "更新できませんでした。"
+      redirect_to  action: :edit
     end
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,34 +6,32 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe 'ユーザー編集' do
+    before do
+      @user = create(:user)
+    end
     context '正常系' do
       it '登録情報編集画面が描画されていること' do
-        user = create(:user)
-        get :edit, params: { id: user.id }
+        get :edit, params: { id: @user.id }
         expect(response).to render_template 'users/edit'
       end
 
       it '正しく値が設定された場合ユーザーのマイページ画面が描画されること' do
-        user = create(:user)
         profile_params = FactoryBot.attributes_for(:profile)
-        patch :update, params: { id: user.id, user: profile_params }
-        expect(response).to redirect_to edit_user_path
+        patch :update, params: { id: @user.id, user: profile_params }
+        expect(response).to redirect_to user_path
       end
 
       it '正しく値が設定された場合ユーザーの情報が更新されていること' do
-        user = create(:user)
-        profile_params = FactoryBot.attributes_for(:profile, name: 'riku', user_id: user.id)
-        patch :update, params: { id: user.id, user: profile_params }
-        expect(user.reload.profile.name).not_to eq 'riku'
+        profile_params = FactoryBot.attributes_for(:profile, name: 'riku', user_id: @user.id)
+        patch :update, params: { id: @user.id, user: profile_params }
+        expect(@user.reload.profile.name).not_to eq 'riku'
       end
     end
     context '異常系' do
       it '正しく値が設定されなかった場合、登録情報編集画面が描画されること' do
-        user = create(:user)
-        profile_params = FactoryBot.attributes_for(:profile, name: nil, user_id: user.id)
-        patch :update, params: { id: user.id, user: profile_params }
-        #TODO　編集画面に遷移しているかの判定
-        expect(response).to redirect_to edit_user_path
+        profile_params = FactoryBot.attributes_for(:profile, name: nil, user_id: @user.id)
+        patch :update, params: { id: @user.id, user: profile_params }
+        expect(response).to redirect_to action: :edit
       end
     end
   end
