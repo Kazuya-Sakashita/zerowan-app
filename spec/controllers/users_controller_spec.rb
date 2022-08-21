a872891176955c5a279e4b696d160de115e3450f
Kazuya-Sakashita <kz0508@gmail.com>
2022-08-21 20:00:22 +0900

    users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,20 +17,20 @@ RSpec.describe UsersController, type: :controller do
 
       it '正しく値が設定された場合ユーザーのマイページ画面が描画されること' do
         profile_params = FactoryBot.attributes_for(:profile)
-        patch :update, params: { id: @user.id, user: profile_params }
+        patch :update, params: { id: @user, user: { profile_attributes: profile_params } }
         expect(response).to redirect_to user_path
       end
 
       it '正しく値が設定された場合ユーザーの情報が更新されていること' do
         profile_params = FactoryBot.attributes_for(:profile, name: 'riku', user_id: @user.id)
-        patch :update, params: { id: @user.id, user: profile_params }
-        expect(@user.reload.profile.name).not_to eq 'riku'
+        patch :update, params: { id: @user, user: { profile_attributes: profile_params } }
+        expect(@user.reload.profile.name).to eq 'riku'
       end
     end
     context '異常系' do
       it '正しく値が設定されなかった場合、登録情報編集画面が描画されること' do
         profile_params = FactoryBot.attributes_for(:profile, name: nil, user_id: @user.id)
-        patch :update, params: { id: @user.id, user: profile_params }
+        patch :update, params: { id: @user, user: { profile_attributes: profile_params } }
         expect(response).to redirect_to action: :edit
       end
     end
