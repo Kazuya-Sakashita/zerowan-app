568eac7dac4f875da01ffde130639b2553db6ab4
Kazuya-Sakashita <kz0508@gmail.com>
2022-09-02 12:03:39 +0900

    sessions_controller_spec.rb修正

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,56 +5,53 @@ RSpec.describe Devise::SessionsController, type: :controller do
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
-  describe 'ログイン、ログアウト' do
+  let!(:user) do
+    create(:user, email: 'test123456789@test.com', password: 'password', password_confirmation: 'password', &:confirm)
+  end
 
-    let!(:user) do
-      create(:user, email: 'test123456789@test.com', password: 'password', password_confirmation: 'password', &:confirm)
+  context 'ログイン画面' do
+    it 'ログイン画面が描画されていること' do
+      get :new
+      expect(response).to render_template 'devise/sessions/new'
+    end
+  end
+
+  context 'ログイン' do
+    before do
+      post :create, params: { user: { email: 'test123456789@test.com', password: 'password' } }
     end
 
-    context 'ログイン画面' do
-      it 'ログイン画面が描画されていること' do
-        get :new
-        expect(response).to render_template 'devise/sessions/new'
-      end
+    it '正しく値が入力された場合、ユーザーのマイページ画面が描画されること' do
+      expect(response).to redirect_to user_path(user)
     end
 
-    context 'ログイン' do
-      before do
-        post :create, params: { user: { email: 'test123456789@test.com', password: 'password' } }
-      end
-
-      it '正しく値が入力された場合、ユーザーのマイページ画面が描画されること' do
-        expect(response).to redirect_to user_path(user)
-      end
-
-      it '正しく値が入力された場合、flash メッセージが正しく表示されること' do
-        expect(flash[:notice]).to eq 'ログインしました。'
-      end
-
-      it '正しく値が入力された場合、ユーザーがログイン状態であること' do
-        expect(controller).to be_user_signed_in
-      end
+    it '正しく値が入力された場合、flash メッセージが正しく表示されること' do
+      expect(flash[:notice]).to eq 'ログインしました。'
     end
 
-    context 'ログアウト' do
-      before do
-        sign_in user
-      end
+    it '正しく値が入力された場合、ユーザーがログイン状態であること' do
+      expect(controller).to be_user_signed_in
+    end
+  end
 
-      it 'Home 画面が描画されること' do
-        delete :destroy
-        expect(response).to redirect_to root_path
-      end
+  context 'ログアウト' do
+    before do
+      sign_in user
+    end
 
-      it 'flash メッセージが正しく表示されること' do
-        delete :destroy
-        expect(flash[:notice]).to eq 'ログアウトしました。'
-      end
+    it 'Home 画面が描画されること' do
+      delete :destroy
+      expect(response).to redirect_to root_path
+    end
 
-      it 'ユーザーがログイン状態ではないこと' do
-        delete :destroy
-        expect(controller).not_to be_user_signed_in
-      end
+    it 'flash メッセージが正しく表示されること' do
+      delete :destroy
+      expect(flash[:notice]).to eq 'ログアウトしました。'
+    end
+
+    it 'ユーザーがログイン状態ではないこと' do
+      delete :destroy
+      expect(controller).not_to be_user_signed_in
     end
   end
 end
