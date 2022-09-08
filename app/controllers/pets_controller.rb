c9fd50fe4ca2217d87e44499cd875a2590cefdf6
Kazuya-Sakashita <kz0508@gmail.com>
2022-09-09 07:45:31 +0900

    確認事項

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,14 +7,18 @@ class PetsController < ApplicationController
 
   def new
     @pet = PetForm.new(user_id: current_user.id)
+    #TODO rubymineで不明なキーワード警告　確認する
 
   end
 
   def create
     @pet = PetForm.new(pet_form_params.merge(user_id: current_user.id))
+    #TODO rubymineで余計な引数がみつかりました警告　確認する
+
     if @pet.save!
       flash[:notice] = "登録完了しました。"
       redirect_to pets_path
+      #TODO showに遷移させようと思ったがidが渡せなかったので一旦、indexに遷移
     else
       render :new, flash[:alert] = "登録できませんでした。"
     end
