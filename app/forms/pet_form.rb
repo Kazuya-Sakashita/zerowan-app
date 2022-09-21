6266ad3942258fd1d1d880f3c0753db8967df88f
Kazuya-Sakashita <kz0508@gmail.com>
2022-09-21 20:39:42 +0900

    pet_form

--- app/forms/pet_form.rb
+++ app/forms/pet_form.rb
@@ -8,13 +8,11 @@ class PetForm
   def save!
 
     return if invalid?
-    binding.pry
+
     @photos = []
     pet_images.reject(&:blank?)&.each do |image|
       @photos << PetImage.new(photo: image, pet_id: pet_id)
     end
-    puts @photos
-    binding.pry
   end
 
   private
