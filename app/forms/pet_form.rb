46b61aca9021d4ffe3cb00c79ae43d3b667b28ea
Kazuya-Sakashita <kz0508@gmail.com>
2022-10-09 20:00:27 +0900

    バリデーション修正

--- app/forms/pet_form.rb
+++ app/forms/pet_form.rb
@@ -6,21 +6,12 @@ class PetForm
   validates :photos, presence: true
 
   validate :validates_number_of_files
-  # FILE_NUMBER_LIMIT = 4 #枚数制限（最大数）
+
 
   def validates_number_of_files
-    binding.pry
-
-    # 枚数に関係なくひかかる、保存されない
-    @photos.present? && @photos.length < 5
-      errors.add(:@photos, '添付枚数は４枚までです。')
-
-    # 枚数で分岐するが保存されない
-    # unless @photos.present? && @photos.length < 5
-    # errors.add(:@photos, '添付枚数は４枚までです。')
-    # end
-
-
+    if photos.present? && photos.length > 4
+      errors.add(:photos, '添付枚数は４枚までです。')
+    end
   end
 
   def initialize(pet_id: nil, pet_images: [])
@@ -33,7 +24,6 @@ class PetForm
   def save!
     # save!,期待値を保存できない場合に例外を発生させる
     # 登録内容に不備がある場合はActiveRecord::RecordInvalid、invalidの場合発生させる
-    binding.pry
     raise ActiveRecord::RecordInvalid.new(self) if invalid?
 
     @photos.each(&:save!)
