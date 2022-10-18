82e8b69147574fc43944bbb6099661e079343084
Kazuya-Sakashita <kz0508@gmail.com>
2022-10-19 07:21:31 +0900

    アイソレーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
   has_one :profile, dependent: :destroy
+  has_one :profile_image, dependent: :destroy
   has_many :pets
 
 
