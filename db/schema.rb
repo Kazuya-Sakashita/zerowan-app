ae9b7099fbb4fbfa4318c662823093f4e60a9e48
Kazuya-Sakashita <kz0508@gmail.com>
2022-10-25 11:58:10 +0900

    profile avatar追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_215955) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_042250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,20 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_215955) do
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
-  create_table "profile_images", force: :cascade do |t|
-    t.string "avatar"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profile_images_on_user_id"
-  end
-
   create_table "profiles", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.datetime "birthday"
     t.string "breeding_experience"
+    t.string "avatar"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -85,5 +78,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_215955) do
 
   add_foreign_key "pet_images", "pets"
   add_foreign_key "pets", "users"
-  add_foreign_key "profile_images", "users"
 end
