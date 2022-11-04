edd8a2a7a87653f5e224418832c1f7cff9787a33
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-04 13:32:25 +0900

    編集ページへと遷移するボタン表示非表示設定

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,5 @@
 class PetsController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, except: [:show, :index]
   def index
     @pets = Pet.all
   end
