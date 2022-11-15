b7e4299991f9d10e7961a2ccc2143da7bbd878d2
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-15 20:01:04 +0900

    area_form　バリデーション追加

--- app/forms/area_form.rb
+++ app/forms/area_form.rb
@@ -3,6 +3,14 @@ class AreaForm
 
   attr_accessor :areas
 
+
+  validate :validates_number_of_areas
+
+  def validates_number_of_areas
+    if 1 > areas.length || areas.length > 47
+      errors.add(:areas, '入力を確認してください。')
+    end
+  end
   def initialize(pet_id: nil, pet_areas: [])
     @areas = []
     pet_areas.reject(&:blank?)&.each do |area|
---
+++ spec/forms/area_form_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe AreaForm, type: :model do
+
+  let!(:user) { create(:user) }
+  let!(:pet) { create(:pet, user_id: user.id) }
+
+  context '1箇所以上、47箇所まで譲渡範囲バリデーション' do
+    context '正常系' do
+      it '譲渡範囲が1箇所であれば有効であること' do
+        areas = []
+        1.times do
+          areas << pet_area = build(:pet_area, pet_id: pet.id)
+        end
+
+        @pet_areas = AreaForm.new
+        @pet_areas.areas = areas
+        @pet_areas.valid?
+        expect(@pet_areas).to be_valid
+      end
+
+      it '譲渡範囲が47箇所であれば有効であること' do
+        areas = []
+        47.times do
+          areas << pet_area = build(:pet_area, pet_id: pet.id)
+        end
+
+        @pet_areas = AreaForm.new
+        @pet_areas.areas = areas
+        @pet_areas.valid?
+        expect(@pet_areas).to be_valid
+      end
+    end
+
+    context '異常系' do
+      it '譲渡範囲が0箇所であれば無効であること' do
+        areas = []
+
+        @pet_areas = AreaForm.new
+        @pet_areas.areas = areas
+        @pet_areas.valid?
+        expect(@pet_areas).not_to be_valid
+        expect(@pet_areas.errors.full_messages).to include('譲渡範囲入力を確認してください。')
+      end
+
+      it '譲渡範囲が48箇所であれば無効であること' do
+        areas = []
+        48.times do
+          areas << pet_area = build(:pet_area, pet_id: pet.id)
+        end
+
+        @pet_areas = AreaForm.new
+        @pet_areas.areas = areas
+        @pet_areas.valid?
+        expect(@pet_areas).not_to be_valid
+        expect(@pet_areas.errors.full_messages).to include('譲渡範囲入力を確認してください。')
+      end
+    end
+  end
+end
