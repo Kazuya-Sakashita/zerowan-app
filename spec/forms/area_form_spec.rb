b127bb5bb6b38672be44dea7230002080df25f38
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-22 12:04:16 +0900

    共通化 修正

--- spec/forms/area_form_spec.rb
+++ spec/forms/area_form_spec.rb
@@ -5,31 +5,26 @@ RSpec.describe AreaForm, type: :model do
   let!(:pet) { create(:pet, user_id: user.id) }
 
   context '1箇所以上、47箇所まで譲渡範囲バリデーション' do
-    let(:areas_1) { build_list(:pet_area, 1, pet_id: pet.id) }
-    let(:areas_47) { build_list(:pet_area, 47, pet_id: pet.id) }
-    let(:areas_0) { build_list(:pet_area, 0, pet_id: pet.id) }
-    let(:areas_48) { build_list(:pet_area, 48, pet_id: pet.id) }
-
     before do
       @pet_areas = AreaForm.new
-      @pet_areas.areas = available_num
+      @pet_areas.areas = areas
       @pet_areas.valid?
     end
     context '譲渡範囲1箇所' do
-      let(:available_num) { areas_1 }
+      let(:areas) { build_list(:pet_area, 1, pet_id: pet.id) }
       it '有効であること' do
         expect(@pet_areas).to be_valid
       end
     end
     context '譲渡範囲47箇所' do
-      let(:available_num) { areas_47 }
+      let(:areas) { build_list(:pet_area, 47, pet_id: pet.id) }
       it '有効であること' do
         expect(@pet_areas).to be_valid
       end
     end
 
     context '譲渡範囲0箇所' do
-      let(:available_num) { areas_0 }
+      let(:areas) { build_list(:pet_area, 0, pet_id: pet.id) }
       it '無効であること' do
         expect(@pet_areas).not_to be_valid
         expect(@pet_areas.errors.full_messages).to include('譲渡範囲入力を確認してください。')
@@ -37,7 +32,7 @@ RSpec.describe AreaForm, type: :model do
     end
 
     context '譲渡範囲48箇所' do
-      let(:available_num) { areas_48 }
+      let(:areas) { build_list(:pet_area, 48, pet_id: pet.id) }
       it '無効であること' do
         expect(@pet_areas).not_to be_valid
         expect(@pet_areas.errors.full_messages).to include('譲渡範囲入力を確認してください。')
