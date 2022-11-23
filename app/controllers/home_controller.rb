934b160af0e299970a1517e68560d6bfe5845ee8
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-24 06:38:03 +0900

    譲渡可能地域の検索 ifを1行にまとめる

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,7 @@ class HomeController < ApplicationController
     @q = Pet.ransack(params[:q])
     @pets = @q.result.includes(:pet_images, :pet_areas)
     # TODO エリアの指定の部分がわからなかったので、下記を追記した。
-    if params[:areas].present?
-      @pets = @pets.where(area_id: params[:areas])
-    end
+    @pets = @pets.where(area_id: params[:areas]) if params[:areas].present?
   end
 
   def search
