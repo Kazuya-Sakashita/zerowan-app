eb59a90e564506f9aa8f62d3b273488fb4099f8e
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-23 20:31:14 +0900

    譲渡可能地域の検索追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   def index
     @q = Pet.ransack(params[:q])
     @pets = @q.result.includes(:pet_images, :pet_areas)
-
+    # TODO エリアの指定の部分がわからなかったので、下記を追記した。
+    if params[:areas].present?
+      @pets = @pets.where(area_id: params[:areas])
+    end
   end
 
   def search
