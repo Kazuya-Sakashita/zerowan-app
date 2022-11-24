9c2f94966e14c1847df68455497cdda3dd85f838
Kazuya-Sakashita <kz0508@gmail.com>
2022-11-24 12:05:09 +0900

    homeのエイリア検索修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   def index
     @q = Pet.ransack(params[:q])
     @pets = @q.result.includes(:pet_images, :pet_areas)
-    # TODO エリアの指定の部分がわからなかったので、下記を追記した。
-    @pets = @pets.where(area_id: params[:areas]) if params[:areas].present?
   end
 
   def search
