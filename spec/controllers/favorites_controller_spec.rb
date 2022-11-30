248e2afa2d997b3f53c3ea6754fc90e25a5cf76a
Kazuya-Sakashita <kz0508@gmail.com>
2022-12-01 07:55:27 +0900

    favorites_controller_spec.rb 途中　redirect_to request.refererの問題？

---
+++ spec/controllers/favorites_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe FavoritesController, type: :controller do
+  describe 'お気に入り機能' do
+    # お気に入り設定するユーザー登録
+    let(:user) do
+      create(:user, email: 'test123456789@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
+    end
+    let(:params) do
+      {
+        pet_id: Pet.last.id,
+        user_id: user.id
+      }
+    end
+
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+
+      create(:user, email: 'pettoukou@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
+      create(:area, place_name: '大阪')
+      create(:pet,
+             category: :dog,
+             petname: 'taro20221101',
+             age: 12,
+             gender: :male,
+             classification: :Chihuahua,
+             introduction: 'おとなしく、賢い',
+             castration: :neutered,
+             vaccination: :vaccinated,
+             recruitment_status: 0,
+             user: User.last)
+      create(:pet_area, pet_id: Pet.last.id, area_id: Area.last.id)
+    end
+
+    it '正しく設定された場合、登録されていること' do
+      sign_in user
+      expect do
+        post :create, params: params
+      end.to change { Favorite.count }.by(1)
+    end
+
+    # it '正しく設定された場合、解除されていること' do
+
+  end
+end
