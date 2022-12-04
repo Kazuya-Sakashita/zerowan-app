24950e901c3166a7a7a7ccddf0838d72f647f019
Kazuya-Sakashita <kz0508@gmail.com>
2022-12-04 10:16:12 +0900

    home画面のペット一覧にページネーション追加

---
+++ spec/features/paginate_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.feature 'ページネーション表示', type: :feature do
+  let(:user) do
+    create(:user, email: 'test123456789@test.com', password: 'password', password_confirmation: 'password', &:confirm)
+  end
+
+  before do
+    create(:area, place_name: '大阪')
+    # TODO pet,pet_areaセットで繰り返しデータ作成方法の書き方を後で考える
+    60.times do
+      create(:pet,
+             category: :dog,
+             petname: 'taro20221101',
+             age: 12,
+             gender: :male,
+             classification: :Chihuahua,
+             introduction: 'おとなしく、賢い',
+             castration: :neutered,
+             vaccination: :vaccinated,
+             recruitment_status: 0,
+             user_id: user.id)
+      create(:pet_area, pet_id: Pet.last.id, area_id: Area.last.id)
+    end
+    visit root_path
+  end
+
+  describe '20件以上登録がある場合' do
+    scenario 'ページネーションが表示されていること' do
+      expect(page).to have_selector '.pagination', text: '1 2 3 Next Last'
+    end
+
+    scenario 'ページネーションが正しく機能すること' do
+      # TODO 評価として'/'が'/?page=2'になるような感じで実施したかったがやり方が見つからなかった
+      # 押した際にページネーションが変化するため、そこをマッチャーで評価とした
+      click_link '2'
+      expect(page).to have_selector '.pagination', text: 'First Previous 1 2 3 Next Last'
+      click_link '3'
+      expect(page).to have_selector '.pagination', text: 'First Previous 1 2 3'
+      click_link 'First'
+      expect(page).to have_selector '.pagination', text: '1 2 3 Next Last'
+      click_link 'Last'
+      expect(page).to have_selector '.pagination', text: 'First Previous 1 2 3'
+    end
+
+  end
+end
