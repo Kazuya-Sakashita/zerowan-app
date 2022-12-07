57750aefb018763086d03e4504528fbb65ceb2a8
Kazuya-Sakashita <kz0508@gmail.com>
2022-12-08 07:10:24 +0900

    お気に入り実装関係の.lastの部分を使用しないように修正

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -5,45 +5,27 @@ RSpec.describe FavoritesController, type: :controller do
     let(:headers){ { 'HTTP_REFERER' => referer } }
     let!(:referer){ "/" }
 
-    # お気に入り設定するユーザー登録
-    let(:user) do
+    let(:customer) do
       create(:user, email: 'test123456789@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
     end
-    let(:params) do
-      {
-        pet_id: Pet.last.id,
-        user_id: user.id
-      }
+
+    let(:pet) do
+      create(:pet)
     end
 
     before do
       @request.env['devise.mapping'] = Devise.mappings[:user]
-
-      pet_own = create(:user, email: 'pettoukou@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
-      own_area = create(:area, place_name: '大阪')
-      own_family = create(:pet,
-             category: :dog,
-             petname: 'taro20221101',
-             age: 12,
-             gender: :male,
-             classification: :Chihuahua,
-             introduction: 'おとなしく、賢い',
-             castration: :neutered,
-             vaccination: :vaccinated,
-             recruitment_status: 0,
-             user: pet_own)
-      create(:pet_area, pet_id: own_family.id, area_id: own_area.id)
-      sign_in user
+      sign_in customer
       request.headers.merge! headers
     end
 
     it '正しく設定された場合、登録されていること' do
-      expect { post :create, params: params }.to change(Favorite, :count).by(1)
+      expect { post :create, params: {user_id: customer.id, pet_id: pet.id} }.to change(Favorite, :count).by(1)
     end
 
     it '正しく設定された場合、解除されていること' do
-      create(:favorite, user_id: user.id)
-      expect { delete :destroy, params: params }.to change(Favorite, :count).by(-1)
+      create(:favorite, user_id: customer.id, pet_id: pet.id )
+      expect { delete :destroy, params: {user_id: customer.id, pet_id: pet.id} }.to change(Favorite, :count).by(-1)
     end
   end
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,35 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
   # お気に入り設定するユーザー登録
-  let(:user) do
+  let(:customer) do
     create(:user, email: 'test123456789@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
   end
 
-  # ユーザー登録、ペット登録
-  before do
-    create(:user, email: 'pettoukou@test.com', password: 'password12345', password_confirmation: 'password12345', &:confirm)
-    create(:area, place_name: '大阪')
-    create(:pet,
-           category: :dog,
-           petname: 'taro20221101',
-           age: 12,
-           gender: :male,
-           classification: :Chihuahua,
-           introduction: 'おとなしく、賢い',
-           castration: :neutered,
-           vaccination: :vaccinated,
-           recruitment_status: 0,
-           user: User.last)
-    create(:pet_area, pet_id: Pet.last.id, area_id:Area.last.id)
+  let(:pet) do
+    create(:pet)
   end
-
 
   describe 'バリデーション' do
     before do
-      create(:favorite, user_id: user.id, pet_id: Pet.last.id)
+      create(:favorite, user_id: customer.id, pet_id: pet.id)
     end
     it 'すでにお気に入りしている場合は、お気に入り登録されないこと' do
-      expect { create(:favorite, user_id: user.id, pet_id: Pet.last.id) }.to raise_error('レコード無効')
+      expect { create(:favorite, user_id: customer.id, pet_id: pet.id) }.to raise_error('レコード無効')
     end
 
   end
