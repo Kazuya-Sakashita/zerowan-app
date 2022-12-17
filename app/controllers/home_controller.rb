0d226a5bfb02f6758979356e34a6ca5dcb27cb25
Kazuya-Sakashita <kz0508@gmail.com>
2022-12-17 15:33:18 +0900

    試しにソート導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @q = Pet.ransack(params[:q])
+    @q.build_sort if @q.sorts.empty?
     @pets = @q.result(distinct: true).includes(:pet_images, :pet_areas).page(params[:page]).per(20)
 
   end
