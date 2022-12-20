2e493aad3a8b38e3656b59c6966b78d11bc2bcd7
Kazuya-Sakashita <kz0508@gmail.com>
2022-12-20 20:01:50 +0900

    sort機能修正中

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @q = Pet.ransack(params[:q])
-    @q.build_sort if @q.sorts.empty?
-    @pets = @q.result(distinct: true).includes(:pet_images, :pet_areas).page(params[:page]).per(20)
-
+    if params[:q].present?
+      @q = Pet.ransack(params[:q])
+      @q.build_sort if @q.sorts.empty?
+      @pets = @q.result(distinct: true).includes(:pet_images, :pet_areas).page(params[:page]).per(20)
+    else
+      params[:q] = { sorts: 'id desc' }
+      @q = Pet.ransack()
+      @pets = Pet.all.page(params[:page]).per(20)
+    end
   end
 
   def search
