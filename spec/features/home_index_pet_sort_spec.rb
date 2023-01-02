c8d2eff5efe31d8265654fd4e064b0f56e1fbdcf
Kazuya-Sakashita <kz0508@gmail.com>
2023-01-02 13:24:11 +0900

    セレクトボックス表示の評価

--- spec/features/home_index_pet_sort_spec.rb
+++ spec/features/home_index_pet_sort_spec.rb
@@ -17,18 +17,7 @@ RSpec.feature 'ソート機能', type: :feature do
     visit root_path
   end
 
-  scenario '新着順に表示されること' do
-    # TODO match %r{#{pet.petname}.*#{other_pet.petname}}この書き方でどうにかしたかったのですが、できませんでした。
-    # expect(have_selector '.petname', visible: false).to match %r{#{pet.petname}.*#{other_pet.petname}}
-
-    expect(page.all('.petname')[0].text).to have_content pet.petname
-    expect(page.all('.petname')[1].text).to have_content other_pet.petname
-  end
-
-  scenario '登録順に表示されること' do
-    select '新着順', from: 'q_sorts'
-    click_button '検索する'
-    expect(page.all('.petname')[0].text).to have_content other_pet.petname
-    expect(page.all('.petname')[1].text).to have_content pet.petname
+  scenario 'セレクトボックスが表示されていること' do
+    expect(page).to have_select('q[sorts]', options: %w!新着順 登録順!)
   end
 end
