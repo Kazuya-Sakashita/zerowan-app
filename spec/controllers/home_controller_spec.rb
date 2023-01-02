9dbeee2d6736f03d77381756e729e20f9e7871c6
Kazuya-Sakashita <kz0508@gmail.com>
2023-01-02 13:26:39 +0900

    ソート機能　コントローラーテストコード検証中

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,4 +7,39 @@ RSpec.describe HomeController, type: :controller do
       expect(response).to render_template '/'
     end
   end
+
+  describe 'home画面' do
+    let!(:user) do
+      create(:user, email: 'test123456789@test.com', password: 'password', password_confirmation: 'password', &:confirm)
+    end
+
+    let!(:pet) do
+      create(:pet)
+    end
+
+    let!(:other_pet) do
+      create(:pet)
+    end
+
+    let!(:params) do
+      {
+        q:
+      {
+        category_eq:  "0",
+        gender_eq:  "",
+        age_lteq:  "",
+        classification_eq:  "",
+        sorts: "id asc"
+      }
+      }
+    end
+
+    it '登録順に表示されること' do
+      get :index, params: params
+      binding.pry
+    end
+
+    '新着順に表示されること'
+
+  end
 end
