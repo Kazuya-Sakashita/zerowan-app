6ba7c4081d2e7905d353184a88fa72f7c07faa02
Kazuya-Sakashita <kz0508@gmail.com>
2023-02-14 12:11:53 +0900

    messages_controller_spec.rb修正　show削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,15 +5,6 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def show
-    @pet = Pet.find(params[:pet_id])
-    @message = Message.new
-    unless @pet.user_id == current_user.id
-      @room = Room.find_or_create_by(user_id: current_user.id, pet_id: @pet.id, owner_id: @pet.user_id)
-      @all_message_exchanges = Message.where(room_id: @room.id).order(id: "DESC")
-    end
-  end
-
   def create
     @pet = Pet.find(params[:pet_id])
     @room = Room.find_by(user_id: current_user.id, pet_id: @pet.id, owner_id: @pet.user_id)
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,33 +11,17 @@ RSpec.describe MessagesController, type: :controller do
       @pet = create(:pet)
       sign_in user
     end
-
-    context 'showアクション' do
-      it 'petへの問い合わせが初めての場合、ルームテーブルに登録' do
-        expect do
-          get :show, params: { pet_id: @pet.id, id: @pet.id }
-        end.to change { Room.count }.by(1)
-      end
-
-      it 'petへの問い合わせが2回目以降の場合、ルームテーブルに登録しない' do
-        @message = get :show, params: { pet_id: @pet.id, id: @pet.id }
-        expect do
-          get :show, params: { pet_id: @pet.id, id: @pet.id }
-        end.to change { Room.count }.by(0)
-      end
-    end
-
-    context 'createアクション' do
+    
+    context 'パラーメーターが正しく設定されて場合' do
       let(:headers){ { 'HTTP_REFERER' => referer } }
-      let!(:referer){"/pets/#{@pet.id}/messages/show"}
+      let!(:referer){"/pets/#{@pet.id}/rooms"}
 
       before do
-        get :show, params: { pet_id: @pet.id, id: @pet.id }
-        @room = Room.last
         request.headers.merge! headers
+        @room = @pet.rooms.find_or_create_by(user_id: user.id, pet_id: @pet.id, owner_id: @pet.user_id)
       end
 
-      it 'パラーメーターが正しく設定されて場合、メッセージが保存できる' do
+      it 'メッセージが保存できること' do
         params = { pet_id: @pet.id , user_id: user.id, message: {body: "はじめまして"}, room_id: @room.id }
         expect do
           post :create, params:params
