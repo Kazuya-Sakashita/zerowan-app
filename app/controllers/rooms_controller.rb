b7a55830f2c806d4a7c9fa28bd0fefc98a6a9c12
Kazuya-Sakashita <kz0508@gmail.com>
2023-02-28 06:32:12 +0900

    rooms_controller.rb修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,6 @@ class RoomsController < ApplicationController
 
   def show
     @message = Message.new
-    user_identification && return
     @room = @pet.rooms.find_or_create_by(user_id: current_user.id, pet_id: @pet.id, owner_id: @pet.user_id)
     @all_message_exchanges = Message.where(room_id: @room.id).order(id: :desc)
   end
