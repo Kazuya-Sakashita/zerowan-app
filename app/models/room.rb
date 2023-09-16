e57f5516053334ac725bbf456e1f55112f9b4c44
Kazuya-Sakashita <kz0508@gmail.com>
2023-09-16 10:43:01 +0900

    Revert "roomモデル修正"
    This reverts commit 74719297d942a5713b0df3dcd766acdf69a9269b.

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,13 +3,11 @@ class Room < ApplicationRecord
   belongs_to :pet
   belongs_to :user
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  has_many :messages, -> { order(created_at: :desc)}
   has_one :latest_message, -> { order(created_at: :desc).limit(1) }, class_name: 'Message'
 
-  validate :different_user_and_owner
-
-  private
-
-  def different_user_and_owner
-    errors.add(:owner_id, :different_user_and_owner) if user_id == owner_id
+  def self.petname_extraction(room_id)
+    @room = Room.find(room_id)
+    @pet = @room.pet
   end
 end
