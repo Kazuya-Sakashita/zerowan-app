08dfd771c5999856f95c71b99260fbf99f819d7a
Kazuya-Sakashita <kz0508@gmail.com>
2023-09-23 22:20:57 +0900

    ユーザーページ追加

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,9 +1,19 @@
 class PetsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
-  def index
-    @pets = Pet.all
+def index
+  @user = User.find(params[:user_id])
+
+  total_pets_count = @user.pets.count
+
+  if params[:all]
+     @pets = @user.pets.all
+  else
+     @pets = @user.pets.limit(4)
   end
+
+  @show_more_link = total_pets_count > 4 && @pets.count < total_pets_count
+end
 
   def new
     # pet ペット情報を保存　petForm 画像を保存で分ける
