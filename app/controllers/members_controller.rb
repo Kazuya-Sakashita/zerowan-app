641fce7539bf9b71ba6bb445adf2accd4e58930e
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-01 07:15:16 +0900

    members/show  petデータ取得preloadに変更

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,10 @@
 class MembersController < ApplicationController
 
   def show
-    @member = User.find(params[:id])
-    @pets = @member.pets.limit(4)
+    member = User.find(params[:id])
+    @pets = Pet.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms).where(user_id:  member.id).limit(4)
 
-    total_pets_count = @member.pets.count
+    total_pets_count = member.pets.count
     @show_more_link = total_pets_count > 4 && @pets.count < total_pets_count
   end
 end
