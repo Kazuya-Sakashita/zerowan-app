c8ebe424ae9797dda299b5fe23b89bdef9469dd9
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-01 07:26:56 +0900

    メンバー投稿一覧にページネーション追加

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,7 +3,9 @@ class PetsController < ApplicationController
 
 def index
     member = User.find(params[:id])
-    @pets = Pet.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms).where(user_id:  member.id)
+    @pets = Pet.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms)
+               .where(user_id:  member.id)
+               .page(params[:page]).per(20)
 end
 
   def new
