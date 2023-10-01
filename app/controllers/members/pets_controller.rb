0ed8d6a1fcaa8b9deffbdc664fb1a6cc25b29fbe
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-01 15:12:34 +0900

    app/controllers/members/pets_controller.rb修正

--- app/controllers/members/pets_controller.rb
+++ app/controllers/members/pets_controller.rb
@@ -2,8 +2,7 @@ module Members
   class PetsController < ApplicationController
     def index
       member = User.find(params[:member_id])
-      @pets = Pet.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms)
-               .where(user_id:  member.id)
+      @pets = member.pets.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms)
                .page(params[:page]).per(20)
     end
   end
