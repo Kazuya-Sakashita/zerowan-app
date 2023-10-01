5797ef76ef1adc348496238a5be6e1f268180646
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-01 17:17:14 +0900

    @show_more_link判定修正

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,7 +4,6 @@ class MembersController < ApplicationController
     member = User.find(params[:id])
     @pets = Pet.preload(:pet_images, :pet_areas, :areas, :user, :favorites, :rooms).where(user_id:  member.id).limit(4)
 
-    total_pets_count = member.pets.count
-    @show_more_link = total_pets_count > 4 && @pets.count < total_pets_count
+    @show_more_link = member.pets.offset(4).exists?
   end
 end
