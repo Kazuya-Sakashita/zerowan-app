dfd9c15bf20a96424d0e04e101b348c7e77216d1
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-22 09:51:50 +0900

    redirect先url修正

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -8,9 +8,9 @@ class Admins::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
-      redirect_to admin_users_path, notice: 'ユーザーが削除されました。'
+      redirect_to admins_users_path, notice: 'ユーザーが削除されました。'
     else
-      redirect_to admin_users_path, alert: 'ユーザー削除に失敗しました。'
+      redirect_to admins_users_path, alert: 'ユーザー削除に失敗しました。'
     end
   end
 end
