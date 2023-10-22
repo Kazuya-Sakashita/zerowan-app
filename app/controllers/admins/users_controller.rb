6114f2d27819bcb2706913fb9e07e2a7660bac6f
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-23 07:37:12 +0900

    管理者機能　ユーザー詳細画面追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -5,6 +5,10 @@ class Admins::UsersController < ApplicationController
     @users = User.includes(%i[profile pets]).page(params[:page]).per(20)
   end
 
+  def show
+    @user = User.includes(%i[profile pets]).find(params[:id])
+  end
+
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
