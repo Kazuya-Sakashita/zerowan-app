8f7ae999efdb7f4bc5b14637874439463c633fe9
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-22 19:36:39 +0900

    userモデル不要な記述削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :trackable, :confirmable
 
   has_one :profile, dependent: :destroy
-
-  # TODO 未実装のため、ユーザー削除時にアイソレーションエラー発生
-  # has_one :profile_image, dependent: :destroy
-
   has_many :pets, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :rooms, dependent: :destroy
-  
 
   accepts_nested_attributes_for :profile, allow_destroy: true
-  # accepts_nested_attributes_for :profile_image, allow_destroy: true
 
   with_options presence: true do
     with_options uniqueness: true do
@@ -25,7 +19,6 @@ class User < ApplicationRecord
   end
 
   def is_owner?
-    self.role == 'owner'
+    role == 'owner'
   end
-
 end
