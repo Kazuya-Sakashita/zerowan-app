07b248cc0975dab48dfbc1f1b5be1970c9a1579f
Kazuya-Sakashita <kz0508@gmail.com>
2023-10-30 07:01:50 +0900

    Pickupのhome/view画面追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @picked_up_pets = Pet.includes(:pet_images, :pet_areas).where(picked_up: true).order(created_at: :desc)
+
     if params[:q].present?
       @q = Pet.ransack(params[:q])
       @pets = @q.result(distinct: true).includes(:pet_images, :pet_areas).page(params[:page]).per(20)
